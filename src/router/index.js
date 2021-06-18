be6193aa8a0697aa6259fee220ba18bfe9c42cff
林晉璿 <hinolin0309@gmail.com>
2021-06-19 01:00:06 +0800

    fix input type

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,41 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: () => import('../views/Front.vue'),
+    children: [
+      {
+        path: 'login',
+        component: () => import('../views/Login.vue')
+      },
+      {
+        path: 'products',
+        component: () => import('../views/Products.vue')
+      },
+      {
+        path: 'product/:id',
+        component: () => import('../views/Product.vue')
+      },
+      {
+        path: 'cart',
+        component: () => import('../views/Cart.vue')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/admin',
+    component: () => import('../views/Dashboard.vue'),
+    children: [
+      {
+        path: 'products',
+        component: () => import('../views/admin/Products.vue')
+      },
+      {
+        path: 'order',
+        component: () => import('../views/admin/Order.vue')
+      }
+    ]
   }
 ]
 
